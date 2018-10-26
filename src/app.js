b377213b59ecaca52e562cfb18c0b8fd96bcbabd
DeuterGraves <puckles@gmail.com>
2018-10-26 16:14:12 +0100

    css and app.js both load.

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// require the stuffs here
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Javascript Loaded - WE'RE FINALLY DOING THIS!!!");
+
+  // call your bind events!
+  // iteration 1 - get data to here... 
+
+});
